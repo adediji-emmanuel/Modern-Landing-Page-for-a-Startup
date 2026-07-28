91e2b3096b750fccca4bcf2e01592ff3f2782a24
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-28 17:00:17 +0100

    Update LandingPage.jsx

--- client/src/pages/LandingPage.jsx
+++ client/src/pages/LandingPage.jsx
@@ -1,1 +1,25 @@
+import Navigation from '../components/Navigation'
+import Hero from '../components/Hero'
+import Features from '../components/Features'
+import HowItWorks from '../components/HowItWorks'
+import Pricing from '../components/Pricing'
+import Testimonials from '../components/Testimonials'
+import CTA from '../components/CTA'
+import Footer from '../components/Footer'
 
+const LandingPage = () => {
+  return (
+    <div className="min-h-screen bg-[#0a0a0f] overflow-x-hidden">
+      <Navigation />
+      <Hero />
+      <Features />
+      <HowItWorks />
+      <Pricing />
+      <Testimonials />
+      <CTA />
+      <Footer />
+    </div>
+  )
+}
+
+export default LandingPage
