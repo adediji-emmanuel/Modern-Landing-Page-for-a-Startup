06f6ec33619a508e45d760903611854ec5a79ecd
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-28 15:55:52 +0100

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,1 +1,27 @@
+import Navbar from './components/Navbar'
+import Hero from './sections/Hero'
+import Features from './sections/Features'
+import HowItWorks from './sections/HowItWorks'
+import Testimonials from './sections/Testimonials'
+import Pricing from './sections/Pricing'
+import CTA from './sections/CTA'
+import Footer from './components/Footer'
 
+function App() {
+  return (
+    <div className="min-h-screen bg-white overflow-x-hidden">
+      <Navbar />
+      <main>
+        <Hero />
+        <Features />
+        <HowItWorks />
+        <Testimonials />
+        <Pricing />
+        <CTA />
+      </main>
+      <Footer />
+    </div>
+  )
+}
+
+export default App
