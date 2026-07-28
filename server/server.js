bb6103870ea4a5d1a55f774dc67f4547fa4000e8
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-28 17:05:26 +0100

    Update server.js

--- server/server.js
+++ server/server.js
@@ -1,1 +1,19 @@
+import app from './src/app.js'
+import { connectDB } from './src/config/database.js'
+import logger from './src/utils/logger.js'
 
+const PORT = process.env.PORT || 5000
+
+const startServer = async () => {
+  try {
+    await connectDB()
+    app.listen(PORT, () => {
+      logger.info(`Server running on port ${PORT}`)
+    })
+  } catch (error) {
+    logger.error('Failed to start server:', error)
+    process.exit(1)
+  }
+}
+
+startServer()
