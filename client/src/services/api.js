2a921a51ed5acd36304f7fd6a42612cab4cbea49
ꜱɢ ᴅᴀʀᴋ 神 <godwithusmustgr8@gmail.com>
2026-07-30 17:05:57 +0100

    Update api.js

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -5,7 +5,7 @@ const api = axios.create({
   headers: {
     'Content-Type': 'application/json',
   },
-})
+});
 
 // Request interceptor for adding auth token
 api.interceptors.request.use((config) => {
